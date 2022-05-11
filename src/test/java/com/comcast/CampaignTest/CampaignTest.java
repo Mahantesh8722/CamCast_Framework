8cb0e3063bc401bd8341ca419c1f0cbfd0c5edbc
new <new@DESKTOP-B7KJV5D>
2022-05-11 17:30:58 +0530

    First Commit with engg 1

---
+++ src/test/java/com/comcast/CampaignTest/CampaignTest.java
@@ -0,0 +1,23 @@
+package com.comcast.CampaignTest;
+
+import org.testng.annotations.Test;
+
+public class CampaignTest {
+
+	@Test(groups = "smokeTest")
+	public void CreateCampaignTest() {
+		System.out.println("===Campaign Created=====");
+		String value1 = System.getProperty("BROWSER");
+		System.out.println(value1);
+	}
+	
+	@Test(groups = "regressionTest")
+	public void ModifyCampaignTest() {
+		System.out.println("===Campaign Modified=====");
+	}
+	
+	@Test(groups = "regressionTest")
+	public void DeleteCampaignTest() {
+		System.out.println("===Campaign Deleted=====");
+	}
+}
